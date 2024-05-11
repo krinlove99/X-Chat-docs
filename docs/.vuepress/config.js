9b2014cb2b64dd878c9e83ac29a56cf7c87370c7
krin <12085564+krinlove@user.noreply.gitee.com>
2024-05-11 10:50:55 +0800

    2024年5月11日10:50:55

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
     bundler: viteBundler(),
-    base: '/X-Chat-docs/docs/',
+    base: '/docs/',
     home:'/1/X-Chat介绍.md',
     footer: 'Copyright © 2024-BASETEC',
     prevLink:true,
