37588b42662a6e2398f5cd789777c3c6c6c2e7d8
krin <krin@krindeMacBook-Pro.local>
2024-09-16 00:20:18 +0800

    修改

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -18,23 +18,14 @@ export default defineConfig({
         text: '快速使用',
         items: [
           { text: '注册账户', link: '/快速使用/注册账户' },
-          { text: 'X-Chat V2 聊天助手', link: '/快速使用/X-Chat V2 聊天助手' },
-          { text: '⚡X-Chat V3 聊天助手', link: '/快速使用/X-Chat V3 聊天助手' },
           { text: '⚡AI实验室', link: '/快速使用/AI实验室' },
-          { text: '合作', link: '/快速使用/企业合作' },
+          { text: '⚡X-Chat AI聊天助手', link: '/快速使用/X-Chat V3 聊天助手' },
+          { text: '会员订阅', link: '/快速使用/price' },
+          { text: 'API开放平台', link: '/快速使用/企业合作' },
         ],
       },
       {
-        text: 'X-Chat V2',
-        items: [
-          { text: 'V2 客户端功能', link: '/V2/V2 客户端功能' },
-          { text: 'V2 模型介绍', link: '/V2/V2 模型介绍' },
-          { text: 'V2 模型进阶功能', link: '/V2/V2 模型进阶功能' },
-          { text: 'V2 错误故障汇总', link: '/V2/V2 错误故障汇总' },
-        ],
-      },
-      {
-        text: 'X-Chat V3',
+        text: 'X-Chat AI聊天',
         items: [
           { text: 'V3 客户端功能', link: '/V3/V3 客户端功能' },
           { text: 'V3 模型介绍', link: '/V3/V3 模型介绍' },
@@ -50,6 +41,15 @@ export default defineConfig({
           { text: 'X-搜索(AI搜索)', link: '/AI实验室/X-搜索' },
           { text: 'AI即时翻译服务', link: '/AI实验室/AI即时翻译服务' },
           { text: 'AI智能语音助手', link: '/AI实验室/aihelper' },
+        ],
+      },
+      {
+        text: 'BASETEC AI(商业版)',
+        items: [
+          { text: 'X-Chat介绍', link: '/V2/enterprise' },
+          { text: 'V2 模型介绍', link: '/V2/V2 模型介绍' },
+          { text: 'V2 模型进阶功能', link: '/V2/V2 模型进阶功能' },
+          { text: 'V2 错误故障汇总', link: '/V2/V2 错误故障汇总' },
         ],
       },
       {
